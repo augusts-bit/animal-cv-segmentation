ae891abc9ca18256e709caf925f373a7169fc767
augusts-bit <55450432+augusts-bit@users.noreply.github.com>
2024-01-09 21:21:34 +0100

    Add files via upload

--- src/yolov8/birds/yolov8_train.py
+++ src/yolov8/birds/yolov8_train.py
@@ -14,6 +14,7 @@ Original file is located at
 # ------------------------------------------------------------------------------------- #
 
 import os, re, random
+import pandas
 import cv2
 from ultralytics import YOLO
 # from utils.datasets import *
@@ -24,6 +25,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import *
 import argparse # Parse arguments given to job
+
+# Get helpers functions
+from helpers.getCategoriesSizes import *
 
 # ==============================================================
 
@@ -39,9 +43,10 @@ parser = argparse.ArgumentParser()
 
 # Training data
 parser.add_argument("--traindata")
-parser.add_argument("--epochs", type=int, default=10)
+parser.add_argument("--epochs", type=int, default=1)
 parser.add_argument("--batch", type=int, default=3)
 parser.add_argument("--lr0", type=float, default=0.00025)
+parser.add_argument("--imgsz", type=int, default=538)
 args = parser.parse_args()
 
 print(os.listdir(args.traindata))
@@ -52,29 +57,16 @@ print(os.listdir(args.traindata))
 
 # ==============================================================
 
-ann_dir = os.path.join(args.traindata, "annotations")
-ann_files = sorted([f for f in os.listdir(ann_dir) if f.endswith('.txt')])
-
-categories = []
-for ann_file in ann_files:
-
-       # Get annotations: bounding box, segmentation (mask) and label
-        with open(os.path.join(ann_dir, ann_file), "r") as file:
-            annotations = file.readlines()
-
-            # Get category
-            for annotation in annotations:
-                # Process each line in the file
-                if annotation.startswith('Original label'):
-
-                    # Extract category
-                    match = re.search(r'Original label for object \d+ : "(.*?)"', annotation)
-                    if match:
-                        label = match.group(1)
-                        categories.append(label)
-
+# ann_files = sorted(os.listdir(os.path.join(args.traindata, "annotations")))
+categories, min_width, min_height = get_classes_sizes(os.path.join(args.traindata, "annotations"))
 soorten = sorted(list(set(categories)))
 print("Dataset contains:", soorten)
+print("Minimum width and heigth:", min_width, "x", min_height) # maybe base rescaling of this
+
+# save dictionary of classes for SAHI
+soorten_dict = {str(index): bird for index, bird in enumerate(soorten)}
+with open('outputs/model_categories.json', 'w') as json_file:
+    json.dump(soorten_dict, json_file)
 
 # ------------------------------------------------------------------------------------- #
 # Split train-validation and create yaml
@@ -90,7 +82,7 @@ IMG_FORMATS = 'bmp', 'dng', 'jpeg', 'jpg', 'mpo', 'png', 'tif', 'tiff', 'webp', 
 
 def img2label_paths(img_paths):
     # Define label paths as a function of image paths
-    sa, sb = f'{os.sep}Images{os.sep}', f'{os.sep}Labels{os.sep}'  # /images/, /labels/ substrings
+    sa, sb = f'{os.sep}images{os.sep}', f'{os.sep}labels{os.sep}'  # /images/, /labels/ substrings
     return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.txt' for x in img_paths]
 
 def autosplit(path, weights, annotated_only=False):
@@ -108,6 +100,15 @@ def autosplit(path, weights, annotated_only=False):
     random.seed(0)  # for reproducibility
     indices = random.choices([0, 1, 2], weights=weights, k=n)  # assign each image to a split
 
+    outloc = "outputs" # change so that it doesn't have to write to BLOB (no permission)
+    txt = ['autosplit_train.txt', 'autosplit_val.txt', ]  # 2 txt files
+
+    print(f'Autosplitting images from {path}' + ', using *.txt labeled images only' * annotated_only)
+    for i, img in zip(indices, files):
+        if not annotated_only or Path(img2label_paths([str(img)])[0]).exists():  # check label
+            with open(os.path.join(outloc, txt[i]), 'a') as f:
+                f.write(f'./{img.relative_to(path.parent).as_posix()}' + '\n')  # add image to txt file
+                
     txt = ['autosplit_train.txt', 'autosplit_val.txt', ]  # 2 txt files
     for x in txt:
         if (path.parent / x).exists():
@@ -119,29 +120,39 @@ def autosplit(path, weights, annotated_only=False):
             with open(path.parent / txt[i], 'a') as f:
                 f.write(f'./{img.relative_to(path.parent).as_posix()}' + '\n')  # add image to txt file
 
-autosplit(os.path.join(args.traindata, "Images"), weights=(0.9, 0.1, 0.0))
+# autosplit(os.path.join(args.traindata, "images"), weights=(0.9, 0.1, 0.0)) # no write access...
 
 data = {
     'path': args.traindata, # root path, start at home directory
-    'train': os.path.join(args.traindata, "autosplit_train.txt"),
-    'val': os.path.join(args.traindata, "autosplit_val.txt"),
-    'nc': 1, # number of classes
-    'names': ['bird']
+    'train': "autosplit_train.txt", # no write access, so store manually
+    'val': "autosplit_val.txt",
+    'nc': len(soorten), # number of classes
+    'names': soorten
 }
 
-with open(os.path.join(args.traindata, "config.yaml"), 'w') as file:
+with open(os.path.join("outputs", "config.yaml"), 'w') as file:
     yaml.dump(data, file, default_flow_style=False)
 
 # ------------------------------------------------------------------------------------- #
 # Train YOLOv8
 # ------------------------------------------------------------------------------------- #
 
+# Check if a GPU is available
+if torch.cuda.is_available():
+    device_name = 'cuda' 
+else:
+    device_name = 'cpu'
+
 # load a pretrained model (recommended for training)
 model = YOLO('yolov8n-seg.pt')
 
 # arguments for training (see https://docs.ultralytics.com/modes/train/#arguments)
-args = {"data": os.path.join(args.traindata, "config.yaml"),
-        "epochs": args.epochs, "batch": args.batch, "lr0": args.lr0,
+args = {"data": os.path.join("outputs", "config.yaml"),
+        "epochs": args.epochs, 
+        "batch": args.batch, 
+        "lr0": args.lr0,
+        "imgsz": args.imgsz,
+        "device": device_name,
         "project": "./outputs", "exist_ok": True} # model will be saved in './outputs' folder in Azure
 
 # train
@@ -150,3 +161,30 @@ model.train(**args)
 # model weights: '/outputs/weights/best.pt'
 # model yaml: '/outputs/args.yaml'
 
+# ------------------------------------------------------------------------------------- #
+# Log best loss
+# ------------------------------------------------------------------------------------- #
+
+# Path to the 'results.csv' file
+results_csv_path = os.path.join('outputs', 'runs', 'segment', 'results.csv')
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv(results_csv_path)
+
+# Remove leading spaces from column names
+df.columns = df.columns.str.strip()
+
+# Specify the loss components you want to consider (YOLOv8 bases best model of a weighted loss, but cannot find it)
+loss_components = ['val/box_loss', 'val/seg_loss', 'val/cls_loss', 'val/dfl_loss']
+
+# Create a new column that contains the sum of the specified loss components
+df['total_loss'] = df[loss_components].sum(axis=1)
+
+# Find the epoch with the minimum total loss
+min_loss_epoch = df[df['total_loss'] == df['total_loss'].min()]['epoch'].values[0]
+
+# Print the minimum total loss and the corresponding epoch
+print(f"Minimum Total Loss: {df['total_loss'].min()} at Epoch {min_loss_epoch}")
+
+# Log
+mlflow.log_metric("total val loss", total_val_loss)
