a77a7422d8c011831245247d6bd7652636366040
augusts-bit <55450432+augusts-bit@users.noreply.github.com>
2024-01-30 22:18:26 +0100

    Add files via upload

--- src/detectron2/birds/helpers/getCategoriesSizes.py
+++ src/detectron2/birds/helpers/getCategoriesSizes.py
@@ -1,16 +1,8 @@
 # %%
 import sys, os, distutils.core
 
-# Detectron and torch (check for correct versions)
-import torch, detectron2
-
 # Parse arguments given to job
 import argparse
-
-# Setup detectron2 logger
-import detectron2
-from detectron2.utils.logger import setup_logger
-setup_logger()
 
 # import some common libraries
 import numpy as np
@@ -21,45 +13,13 @@ import torch
 import tensorboard
 import tabulate
 
-# import some common detectron2 utilities
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2.structures import BoxMode
-from detectron2.data.transforms import RandomFlip
-from detectron2.engine import HookBase
-from detectron2.data import build_detection_train_loader
-import detectron2.utils.comm as comm
-from detectron2.utils.events import get_event_storage
-from detectron2.utils.visualizer import ColorMode
-from detectron2.engine import DefaultTrainer
-from detectron2.evaluation import inference_context
-from detectron2.utils.logger import log_every_n_seconds
-from detectron2.data import DatasetMapper, build_detection_test_loader
-import detectron2.utils.comm as comm
-from detectron2.data import transforms as T
-from sahi.utils.detectron2 import export_cfg_as_yaml
-
-# Other
-from PIL import Image
-from torchvision.io import read_image
-from pycocotools import mask as coco_mask
-import cv2
-import time
-import datetime
-import random
-
-# Log metrics
-import mlflow
-import mlflow.pytorch
-
-def get_classes_sizes(ann_files):
+def get_classes_sizes(ann_dir):
 
   categories = []
   min_width = float('inf')
   min_height = float('inf')
+
+  ann_files = sorted(os.listdir(ann_dir))
 
   for ann_file in ann_files:
 
